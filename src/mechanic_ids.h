1be51b7d519515c10d59d783ffec2c38338b3a6b
Marty Rand <marsedge95@gmail.com>
2018-01-18 22:19:12 -0600

    use const ints instead of #defines for mechanic ids

--- src/mechanic_ids.h
+++ src/mechanic_ids.h
@@ -1,117 +1,117 @@
 #pragma once
 
 //vale guardian
-#define MECHANIC_VG_TELEPORT_GREEN 31860
-#define MECHANIC_VG_TELEPORT_RAINBOW 31392
+const uint16_t MECHANIC_VG_TELEPORT_GREEN = 31860;
+const uint16_t MECHANIC_VG_TELEPORT_RAINBOW = 31392;
 
-#define MECHANIC_VG_GREEN_A 31340//I'm not sure why this mechanic has 4 ids, but it appears to for some reason
-#define MECHANIC_VG_GREEN_B 31391//all these ids are for when 4 people are in the green circle
-#define MECHANIC_VG_GREEN_C 31529//it appears to be a separate id for the 90% hp blast when <4 people are in the green
-#define MECHANIC_VG_GREEN_D 31750
+const uint16_t MECHANIC_VG_GREEN_A = 31340;//I'm not sure why this mechanic has 4 ids, but it appears to for some reason
+const uint16_t MECHANIC_VG_GREEN_B = 31391;//all these ids are for when 4 people are in the green circle
+const uint16_t MECHANIC_VG_GREEN_C = 31529;//it appears to be a separate id for the 90% hp blast when <4 people are in the green
+const uint16_t MECHANIC_VG_GREEN_D = 31750;
 
 //gorseval
-#define MECHANIC_GORS_SLAM 31875
-#define MECHANIC_GORS_EGG 31623
+const uint16_t MECHANIC_GORS_SLAM = 31875;
+const uint16_t MECHANIC_GORS_EGG = 31623;
 
 //sabetha
-#define MECHANIC_SAB_SAPPER_BOMB 31473//Sapper Bomb
-#define MECHANIC_SAB_TIME_BOMB 31485//Time Bomb
-#define MECHANIC_SAB_CANNON 31643//Cannon Barrage
-#define MECHANIC_SAB_FLAMEWALL 31332//Firestorm
+const uint16_t MECHANIC_SAB_SAPPER_BOMB = 31473;//Sapper Bomb
+const uint16_t MECHANIC_SAB_TIME_BOMB = 31485;//Time Bomb
+const uint16_t MECHANIC_SAB_CANNON = 31643;//Cannon Barrage
+const uint16_t MECHANIC_SAB_FLAMEWALL = 31332;//Firestorm
 
 //slothasor
-#define MECHANIC_SLOTH_TANTRUM 34479
-#define MECHANIC_SLOTH_BOMB 34387
-#define MECHANIC_SLOTH_BOMB_AOE 34481
-#define MECHANIC_SLOTH_FLAME_BREATH 34516
-#define MECHANIC_SLOTH_SHAKE 34482
+const uint16_t MECHANIC_SLOTH_TANTRUM = 34479;
+const uint16_t MECHANIC_SLOTH_BOMB = 34387;
+const uint16_t MECHANIC_SLOTH_BOMB_AOE = 34481;
+const uint16_t MECHANIC_SLOTH_FLAME_BREATH = 34516;
+const uint16_t MECHANIC_SLOTH_SHAKE = 34482;
 
 //matthias
-#define MECHANIC_MATT_HADOUKEN_HUMAN 34380
-#define MECHANIC_MATT_HADOUKEN_ABOM 34371
-#define MECHANIC_MATT_SHARD_HUMAN 34404//Shards of Rage
-#define MECHANIC_MATT_SHARD_ABOM 34411//Shards of Rage
-#define MECHANIC_MATT_BOMB 34450//Unstable Blood Magic
-#define MECHANIC_MATT_CORRUPTION 34416//Corruption
-#define MECHANIC_MATT_SACRIFICE 34442//nameless
+const uint16_t MECHANIC_MATT_HADOUKEN_HUMAN = 34380;
+const uint16_t MECHANIC_MATT_HADOUKEN_ABOM = 34371;
+const uint16_t MECHANIC_MATT_SHARD_HUMAN = 34404;//Shards of Rage
+const uint16_t MECHANIC_MATT_SHARD_ABOM = 34411;//Shards of Rage
+const uint16_t MECHANIC_MATT_BOMB = 34450;//Unstable Blood Magic
+const uint16_t MECHANIC_MATT_CORRUPTION = 34416;//Corruption
+const uint16_t MECHANIC_MATT_SACRIFICE = 34442;//nameless
 
 //xera
-#define MECHANIC_XERA_HALF 34883
-#define MECHANIC_XERA_ORB 35128//Temporal Shred
-#define MECHANIC_XERA_ORB_AOE 34913//Temporal Shred
-#define MECHANIC_XERA_MAGIC 35000//Intervention
+const uint16_t MECHANIC_XERA_HALF = 34883;
+const uint16_t MECHANIC_XERA_ORB = 35128;//Temporal Shred
+const uint16_t MECHANIC_XERA_ORB_AOE = 34913;//Temporal Shred
+const uint16_t MECHANIC_XERA_MAGIC = 35000;//Intervention
 
 //carin
-#define MECHANIC_CARIN_TELEPORT 38113
-#define MECHANIC_CARIN_SHARD 38313//Meteor Swarm
-#define MECHANIC_CARIN_GREEN 38302//Spatial Manipulation
+const uint16_t MECHANIC_CARIN_TELEPORT = 38113;
+const uint16_t MECHANIC_CARIN_SHARD = 38313;//Meteor Swarm
+const uint16_t MECHANIC_CARIN_GREEN = 38302;//Spatial Manipulation
 
 //sam
-#define MECHANIC_SAM_SHOCKWAVE 37996
-#define MECHANIC_SAM_SLAP_HORIZONTAL 38168
-#define MECHANIC_SAM_SLAP_VERTICAL 38305//Bludgeon
-#define MECHANIC_SAM_FIXATE_SAM 37868//Fixate: Samarog
+const uint16_t MECHANIC_SAM_SHOCKWAVE = 37996;
+const uint16_t MECHANIC_SAM_SLAP_HORIZONTAL = 38168;
+const uint16_t MECHANIC_SAM_SLAP_VERTICAL = 38305;//Bludgeon
+const uint16_t MECHANIC_SAM_FIXATE_SAM = 37868;//Fixate: Samarog
 
 //deimos
-#define MECHANIC_DEIMOS_OIL 37716
-#define MECHANIC_DEIMOS_SMASH 38208 //smash from the actual boss
-#define MECHANIC_DEIMOS_SMASH_INITIAL 37929 //smash while killing the initial chains
+const uint16_t MECHANIC_DEIMOS_OIL = 37716;
+const uint16_t MECHANIC_DEIMOS_SMASH = 38208; //smash from the actual boss
+const uint16_t MECHANIC_DEIMOS_SMASH_INITIAL = 37929; //smash while killing the initial chains
 
 //Soulless Horror
-#define MECHANIC_HORROR_DONUT_INNER 47327//Vortex Slash
-#define MECHANIC_HORROR_DONUT_OUTER 48432//Vortex Slash ingame, achieve claims it's called "hurricane slash", but it's not
-#define MECHANIC_HORROR_GOLEM_AOE 47430//Soul Rift
-#define MECHANIC_HORROR_PIE_4_A 48363//Quad Slash
-#define MECHANIC_HORROR_PIE_4_B 47915//Quad Slash
-#define MECHANIC_HORROR_SCYTHE 47363//Spinning Slash
+const uint16_t MECHANIC_HORROR_DONUT_INNER = 47327;//Vortex Slash
+const uint16_t MECHANIC_HORROR_DONUT_OUTER = 48432;//Vortex Slash ingame, achieve claims it's called "hurricane slash", but it's not
+const uint16_t MECHANIC_HORROR_GOLEM_AOE = 47430;//Soul Rift
+const uint16_t MECHANIC_HORROR_PIE_4_A = 48363;//Quad Slash
+const uint16_t MECHANIC_HORROR_PIE_4_B = 47915;//Quad Slash
+const uint16_t MECHANIC_HORROR_SCYTHE = 47363;//Spinning Slash
 
 //Dhuum
-#define MECHANIC_DHUUM_GOLEM 48172 //Hateful Ephemera (flesh golem)
-#define MECHANIC_DHUUM_AFFLICTION 47646//Arcing Affliction
-#define MECHANIC_DHUUM_SHACKLE 47335//Soul Shackle
-#define MECHANIC_DHUUM_CRACK 48752//Cull
-#define MECHANIC_DHUUM_MARK 48760//Putrid Bomb
-#define MECHANIC_DHUUM_SUCK_AOE 48398//Cataclysmic Cycle
-#define MECHANIC_DHUUM_TELEPORT_AOE 48176//Death Mark
-#define MECHANIC_DHUUM_SNATCH 47076//nameless
+const uint16_t MECHANIC_DHUUM_GOLEM = 48172; //Hateful Ephemera (flesh golem)
+const uint16_t MECHANIC_DHUUM_AFFLICTION = 47646;//Arcing Affliction
+const uint16_t MECHANIC_DHUUM_SHACKLE = 47335;//Soul Shackle
+const uint16_t MECHANIC_DHUUM_CRACK = 48752;//Cull
+const uint16_t MECHANIC_DHUUM_MARK = 48760;//Putrid Bomb
+const uint16_t MECHANIC_DHUUM_SUCK_AOE = 48398;//Cataclysmic Cycle
+const uint16_t MECHANIC_DHUUM_TELEPORT_AOE = 48176;//Death Mark
+const uint16_t MECHANIC_DHUUM_SNATCH = 47076;//nameless
 
-#define MECHANIC_NIGHTMARE_VOMIT 37030//Toxic Sickness
+const uint16_t MECHANIC_NIGHTMARE_VOMIT = 37030;//Toxic Sickness
 
 //MAMA
-#define MECHANIC_MAMA_WIRL 37408//Blastwave
-#define MECHANIC_MAMA_KNOCK 37391//Tantrum
-#define MECHANIC_MAMA_LEAP 37577//Leap
-#define MECHANIC_MAMA_ACID 37437//Shoot
-#define MECHANIC_MAMA_KNIGHT_SMASH 37185//Explosive Impact
+const uint16_t MECHANIC_MAMA_WIRL = 37408;//Blastwave
+const uint16_t MECHANIC_MAMA_KNOCK = 37391;//Tantrum
+const uint16_t MECHANIC_MAMA_LEAP = 37577;//Leap
+const uint16_t MECHANIC_MAMA_ACID = 37437;//Shoot
+const uint16_t MECHANIC_MAMA_KNIGHT_SMASH = 37185;//Explosive Impact
 
 //Siax
-#define MECHANIC_SIAX_ACID 37477//Vile Spit
+const uint16_t MECHANIC_SIAX_ACID = 37477;//Vile Spit
 
 //Ensolyss
-#define MECHANIC_ENSOLYSS_LUNGE 37154//Lunge
-#define MECHANIC_ENSOLYSS_SMASH_INITIAL 37278//The knockback part
-#define MECHANIC_ENSOLYSS_SMASH 36962//The torment part (up to 5 hits, 1 per player)
+const uint16_t MECHANIC_ENSOLYSS_LUNGE = 37154;//Lunge
+const uint16_t MECHANIC_ENSOLYSS_SMASH_INITIAL = 37278;//The knockback part
+const uint16_t MECHANIC_ENSOLYSS_SMASH = 36962;//The torment part (up to 5 hits, 1 per player)
 
 //Arkk
-#define MECHANIC_ARKK_PIE_A 39685//Horizon Strike (the id appears to be different for each phase of Arkk)
-#define MECHANIC_ARKK_PIE_B 39001//Horizon Strike
-#define MECHANIC_ARKK_PIE_C 39297//Horizon Strike (normal mode)
-#define MECHANIC_ARKK_FEAR 39442//Blinding Radiance
-#define MECHANIC_ARKK_OVERHEAD_SMASH 38844//Overhead Smash
-#define MECHANIC_ARKK_BOMB 38880//Corporeal Reassignment
+const uint16_t MECHANIC_ARKK_PIE_A = 39685;//Horizon Strike (the id appears to be different for each phase of Arkk)
+const uint16_t MECHANIC_ARKK_PIE_B = 39001;//Horizon Strike
+const uint16_t MECHANIC_ARKK_PIE_C = 39297;//Horizon Strike (normal mode)
+const uint16_t MECHANIC_ARKK_FEAR = 39442;//Blinding Radiance
+const uint16_t MECHANIC_ARKK_OVERHEAD_SMASH = 38844;//Overhead Smash
+const uint16_t MECHANIC_ARKK_BOMB = 38880;//Corporeal Reassignment
 
 //conjures
-#define CONJURE_ICE_BOW 5567//Conjure Frost Bow
-#define CONJURE_ICE_BOW_BUFF 15790//Conjure Frost Attributes
+const uint16_t CONJURE_ICE_BOW = 5567;//Conjure Frost Bow
+const uint16_t CONJURE_ICE_BOW_BUFF = 15790;//Conjure Frost Attributes
 
-#define CONJURE_LIGHTNING_HAMMER 5624//Conjure Lightning Hammer
-#define CONJURE_LIGHTNING_HAMMER_BUFF 15791//Conjure Lightning Attributes
+const uint16_t CONJURE_LIGHTNING_HAMMER = 5624;//Conjure Lightning Hammer
+const uint16_t CONJURE_LIGHTNING_HAMMER_BUFF = 15791;//Conjure Lightning Attributes
 
-#define CONJURE_FLAME_AXE 5540//Conjure Flame Axe
-#define CONJURE_FLAME_AXE_BUFF 15789//Conjure Flame Attributes
+const uint16_t CONJURE_FLAME_AXE = 5540;//Conjure Flame Axe
+const uint16_t CONJURE_FLAME_AXE_BUFF = 15789;//Conjure Flame Attributes
 
-#define CONJURE_EARTH_SHIELD 5546//Conjure Earth Shield
-#define CONJURE_EARTH_SHIELD_BUFF 15788//Conjure Earth Attributes
+const uint16_t CONJURE_EARTH_SHIELD = 5546;//Conjure Earth Shield
+const uint16_t CONJURE_EARTH_SHIELD_BUFF = 15788;//Conjure Earth Attributes
 
-#define CONJURE_FIRE_GS 5516//Conjure Fiery Greatsword
-#define CONJURE_FIRE_GS_BUFF 15792//Conjure Fire Attributes
+const uint16_t CONJURE_FIRE_GS = 5516;//Conjure Fiery Greatsword
+const uint16_t CONJURE_FIRE_GS_BUFF = 15792;//Conjure Fire Attributes
