cca5440e4f95f63d72b5e2bb4684dc0503cce8e2
Marty Rand <marsedge95@gmail.com>
2018-10-03 17:45:26 -0500

    Adjust default options window size

--- src/imgui_panels.cpp
+++ src/imgui_panels.cpp
@@ -303,7 +303,7 @@ std::string AppChart::getDefaultExportPath()
 
 void AppOptions::draw(std::vector<Mechanic>* mechanics, const char * title, bool * p_open, ImGuiWindowFlags flags)
 {
-	ImGui::SetNextWindowSize(ImVec2(500, 400), ImGuiSetCond_FirstUseEver);
+	ImGui::SetNextWindowSize(ImVec2(550, 650), ImGuiSetCond_FirstUseEver);
 	ImGui::Begin(title, p_open, flags);
 	ImGui::PushAllowKeyboardFocus(false);
 	
