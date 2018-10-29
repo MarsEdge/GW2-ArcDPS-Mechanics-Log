192ac8a243ea201f41da1ec7ce4153621adff3f4
Marty Rand <marsedge95@gmail.com>
2018-10-29 04:36:56 -0500

    Add empty string check to getPlayer(string)

--- src/Tracker.cpp
+++ src/Tracker.cpp
@@ -48,6 +48,7 @@ Player * Tracker::getPlayer(uintptr_t new_player)
 
 Player * Tracker::getPlayer(std::string new_player)
 {
+	if (new_player.empty()) return nullptr;
 	std::lock_guard<std::mutex> lock(players_mtx);
 	auto it = std::find(players.begin(), players.end(), new_player);
 
