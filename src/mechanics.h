a214d6a8e334fb78a52f9bb83246dace5924edb7
Marty Rand <marsedge95@gmail.com>
2018-12-01 23:30:38 -0600

    Remove unused declaration

--- src/mechanics.h
+++ src/mechanics.h
@@ -79,7 +79,6 @@ struct Mechanic
 };
 
 bool requirementDefault(const Mechanic &current_mechanic, cbtevent* ev, Player* src, Player* dst, Player* current_player);
-bool requirementConjure(const Mechanic &current_mechanic, cbtevent* ev, Player* src, Player* dst, Player* current_player);
 bool requirementDhuumSnatch(const Mechanic &current_mechanic, cbtevent* ev, Player* src, Player* dst, Player* current_player);
 bool requirementBuffApply(const Mechanic &current_mechanic, cbtevent* ev, Player* src, Player* dst, Player* current_player);
 
