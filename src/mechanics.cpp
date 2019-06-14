6c907151d6173ac06270739f113644ce8d907df3
Marty Rand <marsedge95@gmail.com>
2019-06-13 23:01:59 -0500

    Fix qadim2 fixate being wrong mechanic, add earth djinn pillar ripple mechanic

--- src/mechanics.cpp
+++ src/mechanics.cpp
@@ -563,7 +563,10 @@ std::vector<Mechanic>& getMechanics()
 		Mechanic("looked at eye", {56099,56114,56516,56636}, &boss_adina, false, false, verbosity_all, false, true, target_location_dst, 0, 0, -1, -1, ACTV_NONE, CBTB_NONE,true, false, true,
 			requirementDefault, valueDefault, "Diamond Palisade", ""),
 
-		Mechanic("is sniper target",{56510},&boss_qadim2,false,true,verbosity_all,false,false,target_location_dst,2000,0,-1,-1,ACTV_NONE,CBTB_NONE,true,true,true,
+		Mechanic("touched pillar ripple",{56558},&boss_adina,true,false,verbosity_all,false,true,target_location_dst,2000,0,-1,-1,ACTV_NONE,CBTB_NONE,true,true,true,
+			requirementDefault,valueDefault,"Tectonic Upheaval",""),
+
+		Mechanic("is tank",{56510},&boss_qadim2,false,true,verbosity_all,false,false,target_location_dst,0,0,-1,-1,ACTV_NONE,CBTB_NONE,true,true,true,
 			requirementDefault,valueDefault,"Fixated",""),
 
 		Mechanic().setName("got a flux bomb").setIds({MECHANIC_FOTM_FLUX_BOMB}).setFailIfHit(false).setBoss(&boss_fotm_generic),
