82d479468a5607371b9bcc27b13d039a410e2977
Marty Rand <marsedge95@gmail.com>
2019-07-04 21:25:29 -0500

    Add tracking for air djinn bop at phase, qadim2 pylon lava, qadim2 expanding lightning. Fix naming for qadim2 lightning, purple rectangle

--- src/mechanics.cpp
+++ src/mechanics.cpp
@@ -584,14 +584,20 @@ std::vector<Mechanic>& getMechanics()
 		Mechanic("wasn't in bubble",{56372},&boss_sabir,true,false,verbosity_all,false,true,target_location_dst,2000,0,-1,-1,ACTV_NONE,CBTB_NONE,false,false,false,
 			requirementDefault,valueDefault,"Fury of the Storm",""),
 
+		Mechanic("was bopped at phase", {56094},&boss_sabir,true,false,verbosity_all,false,false,target_location_dst,0,0,-1,-1,ACTV_NONE,CBTB_NONE,true,true,true,
+			requirementDefault,valueDefault,"Walloping Wind",""),
+
 		Mechanic("is tank",{56510},&boss_qadim2,false,true,verbosity_all,false,false,target_location_dst,0,0,-1,-1,ACTV_NONE,CBTB_NONE,true,true,true,
 			requirementDefault,valueDefault,"Fixated",""),
 
-		Mechanic("touched lava", {56180,56378},&boss_qadim2,true,false,verbosity_all,false,true,target_location_dst,2000,0,-1,-1,ACTV_NONE,CBTB_NONE,true,true,true,
-			requirementDefault,valueDefault,"Residual Impact",""),//ids are big,small AoEs
+		Mechanic("touched lava", {56180,56378,56541},&boss_qadim2,true,false,verbosity_all,false,true,target_location_dst,2000,0,-1,-1,ACTV_NONE,CBTB_NONE,true,true,true,
+			requirementDefault,valueDefault,"Residual Impact, Pylon Debris Field",""),//ids are big,small(CM),pylon
 
-		Mechanic("was struck by lightning AoE",{56656},&boss_qadim2,true,false,verbosity_all,false,false,target_location_dst,1000,0,-1,-1,ACTV_NONE,CBTB_NONE,true,true,true,
+		Mechanic("was struck by small lightning",{56656},&boss_qadim2,true,false,verbosity_all,false,false,target_location_dst,1000,0,-1,-1,ACTV_NONE,CBTB_NONE,true,true,true,
 			requirementDefault,valueDefault,"Brandstorm Lightning",""),
+		
+		Mechanic("was struck by expanding lightning",{56527},&boss_qadim2,true,false,verbosity_all,false,false,target_location_dst,1000,0,-1,-1,ACTV_NONE,CBTB_NONE,true,true,true,
+			requirementDefault,valueDefault,"Rain of Chaos",""),//TODO: unsure if right id?
 
 		Mechanic("touched arcing line",{56145},&boss_qadim2,true,false,verbosity_all,false,false,target_location_dst,2000,0,-1,-1,ACTV_NONE,CBTB_NONE,true,true,true,
 			requirementDefault,valueDefault,"Chaos Called",""),
@@ -599,7 +605,7 @@ std::vector<Mechanic>& getMechanics()
 		Mechanic("was shockwaved",{56134},&boss_qadim2,true,false,verbosity_all,true,true,target_location_dst,2000,0,-1,-1,ACTV_NONE,CBTB_NONE,true,true,true,
 			requirementDefault,valueDefault,"Force of Retaliation",""),
 
-		Mechanic("touched purple line",{56441},&boss_qadim2,true,false,verbosity_all,false,true,target_location_dst,2000,0,-1,-1,ACTV_NONE,CBTB_NONE,true,true,true,
+		Mechanic("touched purple rectangle",{56441},&boss_qadim2,true,false,verbosity_all,false,true,target_location_dst,2000,0,-1,-1,ACTV_NONE,CBTB_NONE,true,true,true,
 			requirementDefault,valueDefault,"Force of Havoc",""),
 
 		Mechanic("was ran over",{56616},&boss_qadim2,true,false,verbosity_all,false,true,target_location_dst,2000,0,-1,-1,ACTV_NONE,CBTB_NONE,true,true,true,
